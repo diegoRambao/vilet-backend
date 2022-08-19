8e75b397c8ae17c7749f24ad85bb5f118f18a7aa
Diego Rambao <diegoandrestrom04@gmail.com>
2022-08-18 21:59:22 -0500

    agregar cambios en los mensajes de errores

--- src/shared/infrastructure/services/jwt/jwt.service.ts
+++ src/shared/infrastructure/services/jwt/jwt.service.ts
@@ -9,9 +9,6 @@ import {
 export class JwtTokenService implements JwtServiceInterface {
   constructor(private jwtService: JwtService) {}
 
-  checkToken(token: string): Promise<any> {
-    throw new Error('Method not implemented.');
-  }
   createToken(payload: JwtServicePayloadInterface): string {
     return this.jwtService.sign(payload);
   }
